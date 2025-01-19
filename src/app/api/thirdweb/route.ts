103ba5ec0dd75da842d7b544dc6851f59e29e9e2
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2025-01-19 02:27:51 -0300

    feat: implement erc1155MintTo function for minting tokens and update route to handle POST requests

--- src/app/api/thirdweb/route.ts
+++ src/app/api/thirdweb/route.ts
@@ -1,10 +1,16 @@
+import { erc1155MintTo } from "@/lib/thirdweb";
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
+export async function POST() {
   try {
-    const data = null;
-    const error = null;
+    const r4to = "0x1fd1405aE28ef1855A0d26CE07555Be661405fCb";
+    const contractAdress = "0x4D3423981762797Bc0381A6CeFd4D05B8B62bA70";
 
+    const txResult = await erc1155MintTo(contractAdress, r4to, 0);
+
+    console.log("Transaction result:", txResult);
+    const txHash = txResult.transactionHash;
+    const error = null;
     if (error) {
       console.error("Failed to create route:", error);
       return NextResponse.json(
@@ -12,7 +18,7 @@ export async function GET(request: Request) {
         { status: 500 }
       );
     }
-    return NextResponse.json({ data }, { status: 200 });
+    return NextResponse.json({ txHash }, { status: 200 });
   } catch (err) {
     console.error("Error parsing request body:", err);
     return NextResponse.json(
--- src/lib/thirdweb.ts
+++ src/lib/thirdweb.ts
@@ -1,5 +1,11 @@
-import { createThirdwebClient } from "thirdweb";
+import {
+  createThirdwebClient,
+  getContract,
+  prepareContractCall,
+  sendTransaction,
+} from "thirdweb";
 import { optimismSepolia } from "thirdweb/chains";
+import { privateKeyToAccount } from "thirdweb/wallets";
 
 if (!process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID) {
   throw new Error(
@@ -10,5 +16,44 @@ if (!process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID) {
 export const twClient = createThirdwebClient({
   clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID!,
 });
+
+export const twSecretClient = createThirdwebClient({
+  secretKey: process.env.THIRDWEB_SECRET_KEY!,
+});
+
+const personalAccount = privateKeyToAccount({
+  client: twSecretClient,
+  privateKey: process.env.ADMIN_PRIVATE_KEY!,
+});
+
+console.log("Personal account address:", personalAccount.address);
+
+export async function erc1155MintTo(
+  contractAdress: string,
+  to: string,
+  tokenId: number,
+  amount: number = 1,
+  uri: string = ""
+) {
+  const contract = getContract({
+    client: twClient,
+    chain: CHAIN,
+    address: contractAdress,
+  });
+
+  const tx = prepareContractCall({
+    contract,
+    method:
+      "function mintTo(address to, uint256 tokenId, string uri, uint256 amount)",
+    params: [personalAccount.address, BigInt(tokenId), uri, BigInt(amount)],
+  });
+
+  const txResult = await sendTransaction({
+    transaction: tx,
+    account: personalAccount,
+  });
+
+  return txResult;
+}
 
 export const CHAIN = optimismSepolia;
