b08a1e94062b98f39c3a99c955caa8f523d46b48
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2025-01-20 16:03:45 -0300

    feat: add homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 export default function Page() {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 max-w-full h-screen">
-      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-        <div className="aspect-video rounded-xl bg-muted/50" />
-        <div className="aspect-video rounded-xl bg-muted/50" />
-        <div className="aspect-video rounded-xl bg-muted/50" />
+      <div className="min-h-[100vh] flex flex-col justify-center items-center flex-1 rounded-xl bg-muted/50 md:min-h-min">
+        <h1 className="text-6xl font-bold text-lime-400">systēma</h1>
       </div>
-      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
     </div>
   );
 }
