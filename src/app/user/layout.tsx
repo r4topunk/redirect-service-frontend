e3487fa6d2641545c8a4aca1fb0671fcf7b87ea2
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2025-01-21 14:32:26 -0300

    feat: conditionally render ConnectButton based on current pathname

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -1,15 +1,20 @@
+"use client";
+
 import { ConnectButton } from "@/components/connect-button";
 import { ThemeToggle } from "@/components/theme-toggle";
+import { usePathname } from "next/navigation";
 
 export default function UserLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+  console.log({ pathname });
   return (
     <>
       <div className="fixed bottom-2 w-full flex items-end justify-center gap-2">
-        <ConnectButton />
+        {pathname !== "/user/register" && <ConnectButton />}
         <ThemeToggle />
       </div>
       {children}
